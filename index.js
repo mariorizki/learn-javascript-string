47847bf8d929ec2feb485bc14faa44d72c9c47bd
Mario Rizki <mariorizki07@gmail.com>
2019-01-30 08:39:46 +0700

    Create Initial JavaScript

---
+++ index.js
@@ -0,0 +1,25 @@
+const wordA = 'Hello';
+const wordB = 'World';
+
+// Concatination
+console.log(wordA.concat(` ${wordB}`));
+console.log(wordA.concat(' Genius Gon'));
+
+// Upper case
+console.log(wordA.toUpperCase());
+console.log(wordB.toUpperCase());
+
+// Lower case
+console.log(wordA.toLowerCase());
+console.log(wordB.toLowerCase());
+
+// Title case
+
+// function titleCase(str) {
+//   let splitSentence = str.split();
+//   let result = '';
+
+//   return result;
+// }
+
+// console.log(titleCase('my name is gon'));
